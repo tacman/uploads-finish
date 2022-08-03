2f7ccb092fe6fa8359bf17b7f31b4623e0bf3ac4
Tac Tacelosky <tacman@gmail.com>
2022-08-03 12:58:35 -0400

    add load, update rector

--- rector.php
+++ rector.php
@@ -20,11 +20,7 @@ return static function (RectorConfig $rectorConfig): void {
 
     $rectorConfig->rules([
         Rector\Symfony\Rector\MethodCall\GetHelperControllerToServiceRector::class,
-        \Rector\Symfony\Rector\Class_\CommandPropertyToAttributeRector::class,
-        ReturnTypeFromStrictBoolReturnExprRector::class,
-        ReturnTypeFromStrictNewArrayRector::class,
-        ReturnTypeFromStrictScalarReturnExprRector::class,
-        \Rector\Symfony\Rector\MethodCall\GetHelperControllerToServiceRector::class
+cp         \Rector\Symfony\Rector\MethodCall\GetHelperControllerToServiceRector::class
     ]);
 
     // register a single rule
